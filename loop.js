eb2cb39a03341e7afce0bf2fcadaedde99625f6b
Byambasuren11 <byumbasurenbold@gmail.com>
2024-12-06 18:59:04 +0800

    donee

--- loop.js
+++ loop.js
@@ -1,21 +1,27 @@
-const number=3452;
-let sum=0;
-let m=number.toString();
-console.log(m.length)
-for(let i=0; i<=m.length-1; i++){
-   let num=Number(m)
-    sum+=num%10;
-    num=parseInt(num/10);
+// const number=3452;
+// let sum=0;
+// let m=number.toString();
+// for(let i=0; i<=m.length-1; i++){
+//    let num=Number(m)
+//     sum+=num%10;
+//     num=parseInt(num/10);
 
-    console.log(sum)
+//     console.log(sum)
     
+// }
+let register="OG12345678";
+const letter=/\w/;
+let check=register.split();
+console.log(check)
+for(i=0; i<=register.length;i++){
+if(check[1]===letter&& check[2]===letter){}
 }
-console.log(sum)
- const i="*"
- //15
- let n=5;
-let l=0;
- for(let i=n; i<=0; i--){
-    l-=n;
-    console.log(l)
- }
+// console.log(sum);
+//  const i="*"
+//  //15
+//  let n=5;
+// let l=0;
+//  for(let i=n; i<=0; i--){
+//     l-=n;
+//     console.log(l)
+//  }
